b2a8a9bba01895389108004868a3fff3d14d50e5
Mao Michael Xishi <maomich1@teach.cs.toronto.edu>
2020-01-18 00:46:38 -0500

    did some more stuff

--- profile.py
+++ profile.py
@@ -1,11 +1,19 @@
 class profile:
 
     def __init__(self, weight, sex, activity, height, age):
+        activity_levels = \
+        {
+            1: 1.2,
+            2: 1.375,
+            3: 1.55,
+            4: 1.725,
+            5: 1.9
+        }
         self.weight = weight
         self.sex = sex
-        self.activity = activity
         self.height = height
         self.age = age
+        self.activity = activity_levels.get(activity)
 
     def calculate_caloric_need(self):
         BMR = 0
@@ -13,6 +21,6 @@ class profile:
             BMR = 66 + (6.3 * self.weight) + (12.9 * self.height) - (6.8 * self.age)
         else:
             BMR = 665 + (4.3 * self.weight) + (4.7 * self.height) - (4.7 * self.age)
-
+        return BMR * self.activity
 
             
