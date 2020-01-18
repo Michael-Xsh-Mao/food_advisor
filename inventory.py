a92aad4f95a700a9a1e055fdd29f6fc795efc7d3
Animesh <animesh1997@live.com>
2020-01-18 18:58:09 -0500

    merging changes

--- inventory.py
+++ inventory.py
@@ -14,13 +14,13 @@ class inventory:
         self.caloric_consumption = cals
 
     def report_calories(self, days):
-        #reports the number of projected calories remaining in inventory on *days* days from current date
+        #reports the number of projected calories remaining in inventory on days days from current date
         calories_consumed = days * self.caloric_consumption
         calories_remaining = 0
         for item in self.inventory:
             if((item.expiration_date - self.current_date) > 0):
                 calories_remaining += item.getCals()
-        return calories_remaining - calories_consumed
+        return max(calories_remaining - calories_consumed, 0)
 
     def generate_cal_report(self):
         #generates a 10 day caloric report stored as array starting on current day
@@ -31,15 +31,9 @@ class inventory:
 
     def raise_cal_warnings(self):
         cal_report = self.generate_cal_report()
-        for i in range(0, 9):
-            if(cal_report[i] < 2*self.caloric_consumption):
-                if(cal_report[i] < self.caloric_consumption):
-                    print("LESS THAN ONE DAY OF FOOD")
-                else:
-                    print("One day of food remaining")
+        if (cal_report[0] < self.caloric_consumption):
+            print("LESS THAN ONE DAY OF FOOD")
 
-        #continue to raise more warnings etc
-
-
-
-
+        for i in range(1, 9):
+            if(cal_report[i] < self.caloric_consumption): #out of food on this day!
+                print("{} day(s) until out of food!".format())
