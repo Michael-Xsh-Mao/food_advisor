d09dc0b328d49611df9756c1e65574f54457f9e3
Animesh <animesh1997@live.com>
2020-01-18 01:56:28 -0500

    ffs this shoulda been named properly

---
+++ inventory.py
@@ -0,0 +1,15 @@
+from datetime import date
+
+class inventory:
+    daily_intake = 0
+    inventory = []
+    calories_total = 0
+
+
+    def __init__(self):
+        self.current_date = date.today()
+
+
+    def report_calories:
+        for item in inventory:
+                
